badf93f84c4d81841971d007407520f401f9de6a
NishantRout <48598347+NishantRout@users.noreply.github.com>
2019-12-16 17:24:54 +0530

    bugs fixed

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -34,6 +34,11 @@ class AuthService {
   }
 
   static void login(String email, String password) async {
-      _auth.signInWithEmailAndPassword(email: email, password: password);
+    try {
+     await _auth.signInWithEmailAndPassword(email: email, password: password);
+    }
+    catch(e){
+      print(e);
+    }
   }
 }
